5026671e4b75ba2c2e0ef97a6da07c1730038c50
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2022-05-25 19:26:04 +0200

    Update findPositionFixedCulprits.js

--- findPositionFixedCulprits.js
+++ findPositionFixedCulprits.js
@@ -8,15 +8,19 @@ function findCulprits (elem) {
   if (!elem) {
     throw new Error('Could not find element with that selector')
   }
+
   let parent = elem.parentElement
+
   while (parent) {
     const { transform, willChange } = getComputedStyle(parent)
+
     if (transform !== 'none' || willChange === 'transform') {
       console.warn('🚨 Found a culprit! 🚨\n', parent, {
         transform,
         willChange
       })
     }
+
     parent = parent.parentElement
   }
 }
